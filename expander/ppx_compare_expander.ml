64b6ed41765f6f5d6273b4dd29c10a389c9ce1c6
Jeremie Dimino <jdimino@janestreet.com>
2016-12-23 15:32:23 +0000

    114.20+69

--- expander/ppx_compare_expander.ml
+++ expander/ppx_compare_expander.ml
@@ -173,12 +173,7 @@ and compare_variant loc row_fields value1 value2 =
   in
   let e =
     let matched = pexp_tuple ~loc [value1; value2] in
-    match
-      row_fields
-      |> List.rev
-      |> List.map ~f:map
-      |> List.rev
-    with
+    match List.map ~f:map row_fields with
     | [v] -> pexp_match ~loc matched [v]
     | l     ->
       pexp_match ~loc matched
@@ -190,74 +185,23 @@ and compare_variant loc row_fields value1 value2 =
   phys_equal_first value1 value2 e
 
 and branches_of_sum cds =
-  (* process right->left, so gen_symbol call order matches camlp4, thus avoiding
-     spurious diffs *)
-  let cds = List.rev cds in
-  let rightmost_index = 0 in
-  List.mapi cds ~f:(fun i cd ->
-    let rightmost = i = rightmost_index in
-    let loc = cd.pcd_loc in
-    if cd.pcd_res <> None then
-      Location.raise_errorf ~loc "GADTs are not supported by comparelib";
-    match cd.pcd_args with
-    | Pcstr_record lds ->
-      let value1 = gen_symbol ~prefix:"_a" () in
-      let value2 = gen_symbol ~prefix:"_b" () in
-      let res =
-        case ~guard:None
-          ~lhs:(ppat_tuple ~loc [ pconstruct cd (Some (pvar ~loc value1))
-                                ; pconstruct cd (Some (pvar ~loc value2))
-                                ])
-          ~rhs:(compare_of_record_no_phys_equal loc lds (evar ~loc value1) (evar ~loc value2))
-      in
-      if rightmost then
-        [ res ]
-      else
-        let pany = ppat_any ~loc in
-        let pcnstr = pconstruct cd (Some pany) in
-        let case l r n =
-          case ~guard:None ~lhs:(ppat_tuple ~loc [l; r]) ~rhs:(eint ~loc n)
-        in
-        [ res
-        ; case pcnstr pany   (-1)
-        ; case pany   pcnstr 1
-        ]
-    | Pcstr_tuple pcd_args ->
-      match pcd_args with
-      | [] ->
-        let pcnstr = pconstruct cd None in
-        let pany = ppat_any ~loc in
-        let case l r n =
-          case ~guard:None ~lhs:(ppat_tuple ~loc [l; r]) ~rhs:(eint ~loc n)
-        in
-        if rightmost then
-          [ case pcnstr pcnstr 0 ]
-        else
-          [ case pcnstr pcnstr 0
-          ; case pcnstr pany   (-1)
-          ; case pany pcnstr   1
-          ]
-      | tps ->
-        let ids_ty =
-          List.map tps
-            ~f:(fun ty ->
-              (gen_symbol ~prefix:"_a" (),
-               gen_symbol ~prefix:"_b" (),
-               ty))
-        in
-        let lpatt = List.map ids_ty ~f:(fun (l,_r,_ty) -> pvar ~loc l) |> ppat_tuple ~loc
-        and rpatt = List.map ids_ty ~f:(fun (_l,r,_ty) -> pvar ~loc r) |> ppat_tuple ~loc
-        and body =
-          List.map ids_ty ~f:(fun (l,r,ty) ->
-            compare_of_ty ty (evar ~loc l) (evar ~loc r))
-          |> chain_if
-        in
+  let rightmost_index = (List.length cds - 1) in
+  List.concat
+    (List.mapi cds ~f:(fun i cd ->
+      let rightmost = i = rightmost_index in
+      let loc = cd.pcd_loc in
+      if cd.pcd_res <> None then
+        Location.raise_errorf ~loc "GADTs are not supported by comparelib";
+      match cd.pcd_args with
+      | Pcstr_record lds ->
+        let value1 = gen_symbol ~prefix:"_a" () in
+        let value2 = gen_symbol ~prefix:"_b" () in
         let res =
           case ~guard:None
-            ~lhs:(ppat_tuple ~loc [ pconstruct cd (Some lpatt)
-                                  ; pconstruct cd (Some rpatt)
+            ~lhs:(ppat_tuple ~loc [ pconstruct cd (Some (pvar ~loc value1))
+                                  ; pconstruct cd (Some (pvar ~loc value2))
                                   ])
-            ~rhs:body
+            ~rhs:(compare_of_record_no_phys_equal loc lds (evar ~loc value1) (evar ~loc value2))
         in
         if rightmost then
           [ res ]
@@ -270,10 +214,56 @@ and branches_of_sum cds =
           [ res
           ; case pcnstr pany   (-1)
           ; case pany   pcnstr 1
-          ])
-  |> List.map ~f:List.rev
-  |> List.concat
-  |> List.rev
+          ]
+      | Pcstr_tuple pcd_args ->
+        match pcd_args with
+        | [] ->
+          let pcnstr = pconstruct cd None in
+          let pany = ppat_any ~loc in
+          let case l r n =
+            case ~guard:None ~lhs:(ppat_tuple ~loc [l; r]) ~rhs:(eint ~loc n)
+          in
+          if rightmost then
+            [ case pcnstr pcnstr 0 ]
+          else
+            [ case pcnstr pcnstr 0
+            ; case pcnstr pany   (-1)
+            ; case pany pcnstr   1
+            ]
+        | tps ->
+          let ids_ty =
+            List.map tps
+              ~f:(fun ty ->
+                let a = gen_symbol ~prefix:"_a" () in
+                let b = gen_symbol ~prefix:"_b" () in
+                (a, b, ty))
+          in
+          let lpatt = List.map ids_ty ~f:(fun (l,_r,_ty) -> pvar ~loc l) |> ppat_tuple ~loc
+          and rpatt = List.map ids_ty ~f:(fun (_l,r,_ty) -> pvar ~loc r) |> ppat_tuple ~loc
+          and body =
+            List.map ids_ty ~f:(fun (l,r,ty) ->
+              compare_of_ty ty (evar ~loc l) (evar ~loc r))
+            |> chain_if
+          in
+          let res =
+            case ~guard:None
+              ~lhs:(ppat_tuple ~loc [ pconstruct cd (Some lpatt)
+                                    ; pconstruct cd (Some rpatt)
+                                    ])
+              ~rhs:body
+          in
+          if rightmost then
+            [ res ]
+          else
+            let pany = ppat_any ~loc in
+            let pcnstr = pconstruct cd (Some pany) in
+            let case l r n =
+              case ~guard:None ~lhs:(ppat_tuple ~loc [l; r]) ~rhs:(eint ~loc n)
+            in
+            [ res
+            ; case pcnstr pany   (-1)
+            ; case pany   pcnstr 1
+            ]))
 
 and compare_sum loc cds value1 value2 =
   let mcs = branches_of_sum cds in
@@ -397,12 +387,7 @@ let compare_of_td td =
 
 let str_type_decl ~loc ~path:_ (rec_flag, tds) =
   let rec_flag = really_recursive rec_flag tds in
-  let bindings =
-    tds
-    |> List.rev
-    |> List.map ~f:compare_of_td
-    |> List.rev
-  in
+  let bindings = List.map tds ~f:compare_of_td in
   [ pstr_value ~loc rec_flag bindings ]
 
 let sig_type_decl ~loc:_ ~path:_ (_rec_flag, tds) =
